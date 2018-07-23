2c703c80824abb03794e9c541559ca6931fb97f8
Juan Schällibaum <juanschallibaum@hotmail.com>
2018-07-23 16:27:08 -0300

    Update rootkit.c

--- rootkit.c
+++ rootkit.c
@@ -105,7 +105,8 @@ struct proc_dir_entry {
 	char name[];
 };
 
-#elif LINUX_VERSION_CODE >= KERNEL_VERSION(4, 16, 0)
+#elif LINUX_VERSION_CODE >= KERNEL_VERSION(4, 16, 0) && \
+    LINUX_VERSION_CODE < KERNEL_VERSION(4, 17, 0)
 
 struct proc_dir_entry {
 	/*
@@ -133,6 +134,41 @@ struct proc_dir_entry {
 	u8 namelen;
 	char name[];
 };
+
+#elif LINUX_VERSION_CODE >= KERNEL_VERSION(4, 17, 0)
+
+struct proc_dir_entry {
+	/*
+	 * number of callers into module in progress;
+	 * negative -> it's going away RSN
+	 */
+	atomic_t in_use;
+	refcount_t refcnt;
+	struct list_head pde_openers;	/* who did ->open, but not ->release */
+	/* protects ->pde_openers and all struct pde_opener instances */
+	spinlock_t pde_unload_lock;
+	struct completion *pde_unload_completion;
+	const struct inode_operations *proc_iops;
+	const struct file_operations *proc_fops;
+	void *data;
+	unsigned int low_ino;
+	nlink_t nlink;
+	kuid_t uid;
+	kgid_t gid;
+	loff_t size;
+	struct proc_dir_entry *parent;
+	struct rb_root subdir;
+	struct rb_node subdir_node;
+	char *name;
+	umode_t mode;
+	u8 namelen;
+#ifdef CONFIG_64BIT
+#define SIZEOF_PDE_INLINE_NAME	(192-139)
+#else
+#define SIZEOF_PDE_INLINE_NAME	(128-87)
+#endif
+	char inline_name[SIZEOF_PDE_INLINE_NAME];
+}
 
 #endif
 
